e8f454046ed099397710fab638be5309f7a44cac
Timothy Walker <james.and.tisa@gmail.com>
2023-12-24 18:01:50 -0500

    Enable help command

--- src/Commands.ts
+++ src/Commands.ts
@@ -2,5 +2,6 @@ import { SlashCommand } from "./Command";
 import { ping } from "./commands/ping";
 import { battle } from "./commands/battle";
 import { deck } from "./commands/deck";
+import { help } from "./commands/help";
 
-export const SlashCommands: SlashCommand[] = [ping, battle, deck];
+export const SlashCommands: SlashCommand[] = [ping, battle, deck, help];
